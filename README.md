1cd85d5e7d684a6f7142cd89ec6fafda1df6e425
Cyrille Perrot <GutsFun@users.noreply.github.com>
2024-03-11 14:20:13 +0100

    Camera PWA

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# camera-app
-Camera app that runs in your phone’s browser
+## JS CAMERA PWA
+This is a Camera Progressive Web App (PWA) made with pure HTML, CSS, Javascript. Yes, it is installable and serverless - Capable of running offline completely.
+<br><br>
+
+## :camera: SCREENSHOTS
+<p float="left">
+  <img width="250" src="https://github.com/code-boxx/js-camera-pwa/blob/main/assets/ss-js-cam-pwa-1.jpg">
+  <img width="250" src="https://github.com/code-boxx/js-camera-pwa/blob/main/assets/ss-js-cam-pwa-2.jpg">
+  <img width="250" src="https://github.com/code-boxx/js-camera-pwa/blob/main/assets/ss-js-cam-pwa-3.jpg">
+</p><br>
+
+## :ballot_box_with_check: REQUIREMENTS
+1) "Grade A" browser. With support for service worker, indexed database, cache storage.
+2) https:// is required for the features to work.
+3) http://localhost works fine for testing too.
+<br><br>
+
+## :bulb: DOCUMENTATION
+Please visit https://code-boxx.com/javascript-camera-pwa/ for the documentation.
+<br><br>
+
+## :star: SUPPORT
+Like this project? Just give it a star. That will indirectly help grow my blog a little bit. :wink:
+<br><br>
+
+## :newspaper: LICENSE
+Copyright by Code Boxx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- app.js
+++ app.js
@@ -1,33 +1,121 @@
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var cam = {
+  // (A) INIT APP
+  cache : null,  // pictures cache storage
+  aClick : null, // click sound
+  hViews : null, // html screens - [main, gallery, about]
+  hFeed : null,  // html video tag
+  hFlash : null, // html flash screen effect
+  hTimer : null, // html flash screen effect timer
+  init : async () => {
+    // (A1) REQUIREMENTS CHECK - MEDIA DEVICES
+    if (!"mediaDevices" in navigator) {
+      alert("Your browser does not support media devices API.");
+      return;
+    }
 
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
+    // (A2) REQUIREMENTS CHECK - CACHE STORAGE
+    if (!"caches" in window) {
+      alert("Your browser does not support cache storage.");
+      return;
+    }
 
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function (stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-        })
-        .catch(function (error) {
-            console.error("Oops. Something is broken.", error);
-        });
-}
+    // (A3) REQUIREMENTS CHECK - SERVICE WORKER
+    if (!"serviceWorker" in navigator) {
+      alert("Your browser does not support service workers.");
+      return;
+    }
 
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function () {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
+    // (A4) CREATE CACHE STORAGE FOR PICTURES
+    cam.cache = await caches.open("MyPics");
+
+    // (A5) REGISTER SERVICE WORKER
+    try {
+      await navigator.serviceWorker.register("CB-worker.js");
+    } catch (err) {
+      alert("Error registering service worker");
+      console.error(err);
+    }
+
+    // (A6) GET CAMERA PERMISSION + START APP
+    navigator.serviceWorker.ready.then(reg => {
+      navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        // (A6-1) CAMERA CLICK SOUND
+        // https://freesound.org/people/kwahmah_02/sounds/260138/
+        cam.aClick = new Audio("assets/click.mp3");
+  
+        // (A6-2) GET HTML + ENABLE CONTROLS
+        cam.hViews = [
+          document.getElementById("camWrap"),
+          document.getElementById("galWrap"),
+          document.getElementById("aboutWrap")
+        ];
+        cam.hFeed = document.getElementById("camFeed");
+        cam.hFlash = document.getElementById("camFlash");
+        gallery.hPics = document.getElementById("galPics");
+        gallery.hTemplate = document.getElementById("galTemplate").content;
+        cam.hFeed.srcObject = stream;
+        document.getElementById("btnPics").disabled = false;
+        document.getElementById("btnSnap").disabled = false;
+      })
+      .catch(err => {
+        alert("Error initializing camera.")
+        console.error(err);
+      });
+    });
+  },
+
+  // (B) TAKE A SNAPSHOT
+  snap : () => {
+    // (B1) FEEDBACK
+    if (!cam.aClick.ended) {
+      cam.aClick.pause();
+      cam.aClick.currentTime = 0;
+    }
+    cam.aClick.play();
+    clearTimeout(cam.hTimer);
+    cam.hFlash.classList.add("show");
+    cam.hTimer = setTimeout(() => {
+      cam.hFlash.classList.remove("show");
+      clearTimeout(cam.hTimer);
+    }, 100);
+
+    // (B2) CAPTURE VIDEO FRAME TO CANVAS
+    let canvas = document.createElement("canvas"),
+        ctx = canvas.getContext("2d"),
+        vw = cam.hFeed.videoWidth,
+        vh = cam.hFeed.videoHeight;
+    canvas.width = vw;
+    canvas.height = vh;
+    ctx.drawImage(cam.hFeed, 0, 0, vw, vh);
+
+    // (B3) CANVAS TO CACHE STORAGE
+    canvas.toBlob(blob => {
+      let url = URL.createObjectURL(blob);
+      fetch(url).then(async (res) => {
+        // (B3-1) GET NEXT RUNNING NUMBER
+        let i = 1;
+        while (true) {
+          let check = await cam.cache.match("pic-"+i+".png");
+          if (check) { i++; continue; }
+          else { break; }
+        }
+
+        // (B3-2) SAVE IMAGE INTO CACHE
+        cam.cache.put("pic-"+i+".png", res);
+        URL.revokeObjectURL(url);
+      });
+    });
+  },
+
+  // (C) SWITCH BETWEEN PAGES
+  // 0 = main, 1 = gallery, 2 = about
+  view : n => {
+    for (let [i,v] of Object.entries(cam.hViews)) {
+      if (i==n) { v.classList.remove("hide"); }
+      else { v.classList.add("hide"); }
+    }
+    if (n==1) { gallery.list(); }
+  }
 };
-
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+window.addEventListener("load", cam.init);
